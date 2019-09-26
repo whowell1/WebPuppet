9f254bc2f894cd772ce06b19e3fd7c2e0694d39d
Wren <wentarohowell@gmail.com>
2019-09-26 07:09:15 -0400

    working version

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker, withScriptjs, withGoogleMap } from 'google-maps-react';
+
+var placesAPIKey = "AIzaSyCV_5GYLCJmQYZiHFvqRCrNBmrGq5Frqjw";
+var link1 = <a href="https://cutt.ly/dw2EtKI">baby Wren</a>;
+
+
+
+
+
+
 
 
 const mapStyles = {
@@ -11,51 +20,122 @@ export class MapContainer extends Component {
   constructor(props) {
     super(props);
   }
-
-state = {
-    stores: [
-    
-            {latitude: 33.7490, longitude: -84.3880},
-            {latitude:38.9108, longitude:-75.5277},
-            {latitude:34.007, longitude:-81.0348},
-            {latitude: 24.4539, longitude:54.3773},
-            {latitude: 40.7657, longitude:140.9176},
-            {latitude: 35.2271, longitude:-80.8431}
-            
-            /////////////////////////////
-            
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
+    randomFact: {}
+  };
 
 
-          ]
-        }
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true,
+    });
 
-  displayMarkers = () => {
-    return this.state.stores.map((store, index) => {
-      return <Marker key={index} id={index} position={{
-       lat: store.latitude,
-       lng: store.longitude
-     }}
-     onClick={() => console.log("You clicked me!")} />
-    })
-  }
-  // state = {
-  //   showingInfoWindow:false,
-  //   activeMarker: {},
-  //   selectedPlace: {}
-  // }
+  onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  };
   render() {
     return (
-      <Map
-      google={this.props.google}
-      zoom={2}
-      style={mapStyles}
-      initialCenter={{ lat: 47.444, lng: 130.176}}
-    >
-      <Marker position={{ lat: 35.443, lng: 139.63}} />
 
-      {this.displayMarkers()}
+       <Map google={this.props.google} zoom={3}
+          onClick={this.onMapClicked}>
 
-    </Map>
+<Marker onClick={this.onMarkerClick}
+      position ={{lat:35.4437, lng: 139.6380 }}
+        name = {"This is the start of my story. Every story has origin story not matter how insignificant. I have very little memories here."}
+        /> 
+        <InfoWindow
+        marker={this.state.activeMarker}
+        visible={this.state.showingInfoWindow}>
+          <div>
+            <p>{this.state.selectedPlace.name}</p>
+          </div>
+      </InfoWindow>
+
+
+
+      <Marker onClick={this.onMarkerClick}
+      position ={{lat:33.7490, lng: -84.3880 }}
+        name = {"Georgia Tech, where the adventures continue. Atlanta Skyline: https://cutt.ly/aw2Ejp6"}
+        /> 
+        <InfoWindow
+        marker={this.state.activeMarker}
+        visible={this.state.showingInfoWindow}>
+          <div>
+            <p>{this.state.selectedPlace.name} + link1</p>
+          </div>
+      </InfoWindow>
+
+      <Marker onClick={this.onMarkerClick}
+      position ={{lat:38.9108, lng: -75.5277 }}
+        name = {"The northern years. https://cutt.ly/Bw2EJwB"}
+        /> 
+        <InfoWindow
+        marker={this.state.activeMarker}
+        visible={this.state.showingInfoWindow}>
+          <div>
+            <p>{this.state.selectedPlace.name}</p>
+          </div>
+      </InfoWindow>
+
+      <Marker onClick={this.onMarkerClick}
+
+      position ={{lat:34.007, lng: -81.0348 }}
+        name = {"The sad formative years of my life. https://cutt.ly/Fw2ECfa"}
+        /> 
+        <InfoWindow
+        marker={this.state.activeMarker}
+        visible={this.state.showingInfoWindow}>
+          <div>
+            <p>{this.state.selectedPlace.name}</p>
+          </div>
+      </InfoWindow>
+
+      <Marker onClick={this.onMarkerClick}
+      position ={{lat:24.4539, lng: 54.3773 }}
+        name = {"The cultural excursions. Burj Al-Arab: https://cutt.ly/nw2E9EV"}
+        /> 
+        <InfoWindow
+        marker={this.state.activeMarker}
+        visible={this.state.showingInfoWindow}>
+          <div>
+            <p>{this.state.selectedPlace.name}</p>
+          </div>
+      </InfoWindow>
+      <Marker onClick={this.onMarkerClick}
+      position ={{lat:40.7657, lng: 140.9176}}
+        name = {"Living in Asia. This is https://cutt.ly/0w2RwQO"}
+        /> 
+        <InfoWindow
+        marker={this.state.activeMarker}
+        visible={this.state.showingInfoWindow}>
+          <div>
+            <p>{this.state.selectedPlace.name}</p>
+          </div>
+      </InfoWindow>
+
+      <Marker onClick={this.onMarkerClick}
+      position ={{lat:35.2271,lng: -80.8431}}
+        name = {"Summer Days. https://cutt.ly/Nw2Ruak"}
+        /> 
+        <InfoWindow
+        marker={this.state.activeMarker}
+        visible={this.state.showingInfoWindow}>
+          <div>
+            <p>{this.state.selectedPlace.name}</p>
+          </div>
+      </InfoWindow>    
+          </Map>
+
 );
   }
 }
@@ -65,33 +145,6 @@ export default GoogleApiWrapper({
 })(MapContainer);
 
 
-// import React, {Component} from 'react';
-// import { GoogleApiWrapper, InfoWindow, Map, Marker } from 'google-maps-react';
 
-// const mapStyles = {
-//   width: '100%',
-//   height: '100%'
-// };
-
-// export class MapContainer extends Component {
-//   render() {
-//     return (
-//       <Map
-//         google={this.props.google}
-//         zoom={14}
-//         style={mapStyles}
-//         initialCenter={{
-//          lat: -1.2884,
-//          lng: 36.8233
-//         }}
-//       />
-//     );
-//   }
-// }
-
-
-// export default GoogleApiWrapper({
-//   apiKey: 'AIzaSyAoRYLBYIz5qKDfuJnfBGVy-QG-0RLswTw'
-// })(GoogleMapsContainer);
 
 
